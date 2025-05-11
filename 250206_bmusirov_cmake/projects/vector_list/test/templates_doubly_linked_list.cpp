3424486324f1e191dfe322e03f077fbdd7805fc0
VickersSnickers <trojanhorsecard@gmail.com>
2025-05-11 14:14:43 +0300

    vector_list added

---
+++ 250206_bmusirov_cmake/projects/vector_list/test/templates_doubly_linked_list.cpp
@@ -0,0 +1,6 @@
+#include "e:\ProgramFiles\Projects\C++GitReps\kmms_bmusirov_homeworks\250206_bmusirov_cmake\projects\vector_list\src\doubly_linked_list.hpp"
+#include "e:\ProgramFiles\Projects\C++GitReps\kmms_bmusirov_homeworks\250206_bmusirov_cmake\projects\vector_list\src\doubly_linked_list.cpp"
+
+template class mbr::DoublyLinkedList<int>;
+template class mbr::DoublyLinkedList<double>;
+template class mbr::DoublyLinkedList<char>;
---
+++ 250206_bmusirov_cmake/projects/vector_list/test/templates_vector.cpp
@@ -0,0 +1,6 @@
+#include "e:\ProgramFiles\Projects\C++GitReps\kmms_bmusirov_homeworks\250206_bmusirov_cmake\projects\vector_list\src\vector.hpp"
+#include "e:\ProgramFiles\Projects\C++GitReps\kmms_bmusirov_homeworks\250206_bmusirov_cmake\projects\vector_list\src\vector.cpp"
+
+template class mbr::Vector<int>;
+template class mbr::Vector<double>;
+template class mbr::Vector<char>;
